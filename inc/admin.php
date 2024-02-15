297614690fe11f96141209410596c161af28f077
fontaholic <fontaholic@users.noreply.github.com>
2024-02-15 11:42:24 -0500

    Update admin.php
    added choir_locations and playlists and minimized the unruly code

--- inc/admin.php
+++ inc/admin.php
@@ -2,31 +2,19 @@
 
 defined( 'ABSPATH' ) or die;
 
-// change page link to display published pages only
-// https://wordpress.stackexchange.com/questions/91299/how-to-display-by-default-only-published-posts-pages-in-the-admin-area
-function wcs_change_admin_page_link() {
+// Change link to display published items only
+function wcs_change_admin_link( $post_type, $menu_slug ) {
 	global $submenu;
-	$submenu['edit.php?post_type=page'][5][2] = 'edit.php?post_type=page&post_status=publish';
+	$submenu["edit.php?post_type=$post_type"][5][2] = "edit.php?post_status=publish&post_type=$post_type";
 }
-add_action( 'admin_menu', 'wcs_change_admin_page_link' );
+add_action('admin_menu', 'wcs_change_admin_links');
 
-// change post link to display published posts only
-function wcs_change_admin_post_link() {
-	global $submenu;
-	$submenu['edit.php'][5][2] = 'edit.php?post_status=publish';
+// Change choir_location link to display published locations only
+function wcs_change_admin_links( ) {
+	wcs_change_admin_link( 'page', 'page' );
+	wcs_change_admin_link( 'post', 'edit' );
+	wcs_change_admin_link( 'product', 'product' );
+	wcs_change_admin_link( 'elementor_library', 'elementor_library' );
+	wcs_change_admin_link( 'choir_location', 'choir_location' );
+	wcs_change_admin_link( 'playlist', 'playlist' );
 }
-add_action( 'admin_menu', 'wcs_change_admin_post_link' );
-
-// change post link to display published products only (i made this one!)
-function wcs_change_admin_product_link() {
-	global $submenu;
-	$submenu['edit.php?post_type=product'][5][2] = 'edit.php?post_type=product&post_status=publish';
-}
-add_action( 'admin_menu', 'wcs_change_admin_product_link' );
-
-// change elementor templates link to display published templates only (i made this one!)
-function wcs_change_admin_elementor_templates_link() {
-	global $submenu;
-	$submenu['edit.php?post_type=elementor_library'][5][2] = 'edit.php?post_status=publish&post_type=elementor_library';
-}
-add_action( 'admin_menu', 'wcs_change_admin_elementor_templates_link' );
